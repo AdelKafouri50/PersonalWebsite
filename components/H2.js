cbbc3f5deb86f3b3bf01a22b6c2b36fbb4e27d5a
Adel Kafouri <AdelKafouri50@gmail.com>
2023-07-16 00:41:17 +0300

    fixed issue with weight loss article

--- components/H2.js
+++ components/H2.js
@@ -1,25 +1,25 @@
-// function getAnchor(text) {
-//   if (text.toLowerCase()){
-//     return text
-//       .toLowerCase()
-//       .replace(/[^a-z0-9 ]/g, '')
-//       .replace(/[ ]/g, '-');
-//   }
-//   else{
-//     return text
-//   }
-//   }
-//   const H2 = ({ children }) => {
-//     const anchor = getAnchor(children);
-//     const link = `#${anchor}`;
-//     return (
-//       <h2 id={anchor} className="relative mt-3 border-t-2 border-rose-200/5 pt-9 2xl:text-5xl text-white mb-2 2xl:mb-6 font-bold sm:text-3xl">
-//         <a href={link} className="anchor-link">
+function getAnchor(text) {
+  if (text.toLowerCase()){
+    return text
+      .toLowerCase()
+      .replace(/[^a-z0-9 ]/g, '')
+      .replace(/[ ]/g, '-');
+  }
+  else{
+    return text
+  }
+  }
+  const H2 = ({ children }) => {
+    const anchor = getAnchor(children);
+    const link = `#${anchor}`;
+    return (
+      <h2 id={anchor} className="relative mt-3 border-t-2 border-rose-200/5 pt-9 2xl:text-5xl text-white mb-2 2xl:mb-6 font-bold sm:text-3xl">
+        <a href={link} className="anchor-link">
           
-//         </a>
-//         {children}
-//       </h2>
-//     );
-//   };
-//   export default H2;
+        </a>
+        {children}
+      </h2>
+    );
+  };
+  export default H2;
 
