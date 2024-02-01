5abafa9fd04fb34340e24338e3224cbb06f3b6ac
Adel Kafouri <AdelKafouri50@gmail.com>
2024-02-01 12:59:59 +0200

    added vercel analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { Analytics } from '@vercel/analytics/react';
 function Layout({ children }) {
   return (
     <div className="w-fit lg:w-auto ">
       <Header />
-      <main>{children}</main>
+      <main>
+        {children}
+        <Analytics />  
+      </main>
       <Footer />
     </div>
   );
